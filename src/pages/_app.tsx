ef257cb7b781a2b2daaf099f4a5467b471b8cba5
Rafał <justregularusersrsly@gmail.com>
2025-07-31 13:08:45 +0200

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 // @ts-nocheck
+/* eslint-disable */
 // global styles
 import "../assets/css/styles.scss";
 import "swiper/swiper.scss";
--- src/pages/product/[pid].tsx
+++ src/pages/product/[pid].tsx
@@ -1,4 +1,5 @@
 // @ts-nocheck
+/* eslint-disable */
 import type { GetServerSideProps } from "next";
 import { useState } from "react";
 
